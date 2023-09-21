05bbed74866c4e819e6de9ec4b11a17b9028ac22
RyanSoares7 <ryansoareshub@gmail.com>
2023-09-21 08:41:56 -0300

    Set host option on listen 0.2

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
-// import { createServer } from 'node:http';
-
-// const server = createServer((request, response) => {
-//   response.write('Hello World');
-//   return response.end();
-// });
-
-// server.listen(3333);
-
 import { fastify } from 'fastify';
 
-// import { DataBaseMemory } from './database-memory.js';
 import { DataBasePostgres } from './database-postgres.js';
 
 const server = fastify();
 
-// const database = new DataBaseMemory();
 const database = new DataBasePostgres();
 
 server.post('/videos', async (request, reply) => {
